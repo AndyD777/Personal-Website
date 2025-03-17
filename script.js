f1165079a6aef92d44d574bb683858aff869503f
Andrew Depradine <andy.depradine@yahoo.com>
2025-03-17 18:46:46 -0400

    Updating Files

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Hello, World!"); //Outputs "Hello, World" to the console
+
+const form = document.querySelector('#form')
+const submitButton = document.querySelector('#submit')
+
+form.addEventListener('submit', (e) => {
+  submitButton.disabled = true
+  e.preventDefault()
+  window.location.href = window.location.origin + '/success.html'
+})
